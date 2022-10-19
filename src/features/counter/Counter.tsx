3011773d17042e29eb45e1f7a2b8df491ae5a002
Anton Bakum <studyantonbakum@gmail.com>
2022-10-19 11:53:56 +0300

    Own Thunk added

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -8,6 +8,7 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  Multiply
 } from './counterSlice';
 import styles from './Counter.module.css';
 
@@ -61,6 +62,12 @@ export function Counter() {
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
           Add If Odd
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => dispatch(Multiply(incrementValue))}
+        >
+          Multiply
         </button>
       </div>
     </div>
