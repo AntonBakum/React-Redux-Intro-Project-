806284e273a53858441a096be70c8878620932c1
Anton Bakum <studyantonbakum@gmail.com>
2022-11-21 09:57:13 +0200

    adding changes has been resolved

--- pet-api/pet-api/Controllers/WeatherForecastController.cs
+++ pet-api/pet-api/Controllers/WeatherForecastController.cs
@@ -9,14 +9,7 @@ namespace pet_api.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        };
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
