1c6e1117eb9a171752ac066ad89eb08e6d0eecf4
Anton Bakum <studyantonbakum@gmail.com>
2022-12-19 20:00:21 +0200

    changing edit-delete-has been resolved

--- src/validationSchemas/createValidationSchema.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-
-const inputPattern = /^([^0-9]*)$/
-export const createValidationSchema = yup.object().shape({
-    name: yup.string().matches(inputPattern, "Name should contain letters only").min(3, "Name should be of minimum 3 characters length").max(50, "Name should be of maximum 50 characters length").required("Name is required"),
-    description: yup.string().max(300, "Description should be of maximum 50 characters length").required("Description is required"),
-})
